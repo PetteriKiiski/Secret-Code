9d19d8f2e4507e63e76a9ed46aa038c52a84c8df
Petteri Kiiski <petteri.daniel.kiiski@gmail.com>
2021-06-02 18:23:42 -0700

    Checking on Tetris

--- SecretCode.py
+++ SecretCode.py
@@ -23,6 +23,32 @@ WhiteBead = pygame.image.load("Images/WhiteBead.png")
 YellowBead = pygame.image.load("Images/YellowBead.png")
 #Clues
 clues = []
+#Actions
+def RedButton(): #RedButton Action
+	pass
+def OrangeButton(): #OrangeButton Action
+	pass
+def YellowButton(): #YellowButton Action
+	pass
+def GreenButton(): #GreenButton Action
+	pass
+def BlueButton(): #BlueButton Action
+	pass
+def PurpleButton(): #PurpleButton Action
+	pass
+def PinkButton(): #PinkButton Action
+	pass
+def WhiteButton(): #WhiteButton Action
+	pass
+#Buttons
+buttons = {pygame.Rect(475, 100, 50, 50):RedButton, \
+	pygame.Rect(475, 150, 50, 50):OrangeButton, \
+	pygame.Rect(475, 200, 50, 50):YellowButton, \
+	pygame.Rect(475, 250, 50, 50):GreenButton, \
+	pygame.Rect(475, 300, 50, 50):BlueButton, \
+	pygame.Rect(475, 350, 50, 50):PurpleButton, \
+	pygame.Rect(475, 400, 50, 50):PinkButton, \
+	pygame.Rect(475, 450, 50, 50):WhiteButton}
 #Infinite Loop
 while True:
 	#Clear screen
@@ -30,6 +56,14 @@ while True:
 	#Display
 	canvas.blit(Title, (0, 0))
 	canvas.blit(SideBar, (400, 0))
+	canvas.blit(pygame.Rect(475, 100))
+	canvas.blit(pygame.Rect(475, 150))
+	canvas.blit(pygame.Rect(475, 200))
+	canvas.blit(pygame.Rect(475, 250))
+	canvas.blit(pygame.Rect(475, 300))
+	canvas.blit(pygame.Rect(475, 350))
+	canvas.blit(pygame.Rect(475, 400))
+	canvas.blit(pygame.Rect(475, 450))
 	for x in range(110, 370, 80):
 		for y in range(100, 600, 50):
 			canvas.blit(BigHole, (x, y))
@@ -42,4 +76,9 @@ while True:
 		if event.type == QUIT:
 			pygame.quit()
 			sys.exit()
+		if event.type == MOUSEBUTTONDOWN:
+			pos = pygame.mouse.get_pos()
+			for button in list(buttons.keys()):
+				if button.collidepoint(pos):
+					buttons[button]()
 	pygame.display.update()
